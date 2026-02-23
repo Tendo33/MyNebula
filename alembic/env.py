e7d1516cb9fe0558f17822655c116b05910132d5
Tendo33 <sjf1998112@gmail.com>
2026-02-23 17:47:38 +0800

    chore: bump version to 1.0.1 and add db migration

--- alembic/env.py
+++ alembic/env.py
@@ -1,7 +1,11 @@
 """Alembic migration environment configuration."""
 
 import asyncio
+import sys
 from logging.config import fileConfig
+
+if sys.platform == "win32":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
